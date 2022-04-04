b84259d51e17235c13150443caaff62587b1dfdf
Tejas Sudarshan <59815659+tejsudarshan@users.noreply.github.com>
2022-04-04 18:50:02 +0530

    Create test.v

---
+++ test.v
@@ -0,0 +1,15 @@
+module test();
+reg clk,reset;
+wire [3:0] counter;
+
+UPC dut(clk,reset,counter);
+initial begin
+clk=0;
+forever #5 clk=~clk;
+end
+initial begin
+reset=1;
+#20;
+reset=0;
+end
+endmodule
